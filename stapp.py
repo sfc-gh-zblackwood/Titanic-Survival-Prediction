a07907ba52d29b764c1c3dcf6d41fdeae921b700
KeeObom <melunatam4@gmail.com>
2024-09-20 19:04:50 +0100

    deleted pipfile.lock

--- stapp.py
+++ stapp.py
@@ -9,11 +9,11 @@ import os
 
 
 # Load the trained model
-# model = joblib.load('titanic_survival_model.pkl')
-import pickle
+model = joblib.load('titanic_survival_model.pkl')
+# import pickle
 
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# with open('model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
 
 # Function to make a prediction based on user input
